b4bb00c3d907a472a7306c272f8e210a42b56273
Efrén Juárez-Castillo <46583863+juarezefren@users.noreply.github.com>
2023-02-02 13:13:59 -0600

    clase PushButton
    Ejecuta una función cuando se presiona un push button, el push button sebe estar conectado asi;
    terminal uno a tierra
    terminal 2 a resistencia de 1 a 2K y de ahi a 3.3 v
    Entre la terminal 2 y la resistencia se toma la entrada a un pin digital

---
+++ PushButton.h
@@ -0,0 +1,62 @@
+class PushButton {
+   private:
+    int _buttonState;
+    int _pinButton;
+    std::function<void(void)> _callback;
+
+  public:
+    /**
+     * Constructor de la clase PushButton
+     * recibe como parámetro el PIN al
+     * cual se encuentra conectado el botón
+     */
+    CButton(int pinButton) : _pinButton(pinButton) {
+      pinMode(_pinButton, INPUT_PULLUP); // Lo configura de salida
+    }
+
+    /**
+     * Esta función recibe como parámetro la función fp()
+     * y la asigna a una función lambda, será la función que se
+     * ejecute cuando se presione el botón
+     */
+    void attachFunction(std::function<void(void)> callback) {
+      _callback = callback;
+    }
+
+    /**
+     * Esta función actualiza el estado del botón y
+     * ejecuta la función asociada si el botón es presionado
+     */
+    void update() {
+      int newState = digitalRead(_pinButton);
+      if (newState == LOW && _buttonState == HIGH) {
+        _callback();
+      }
+      _buttonState = newState;
+    }
+};
+
+
+
+/*
+#include "PushButton.h"
+
+int cont=0;
+PushButton boton(D0);
+
+void funcion(){
+  cont++;
+  Serial.println("Se presiono el botón: " + String(cont));
+}
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);  
+  boton.attachFunction(funcion);
+}
+
+void loop() {
+  boton.update();
+}
+  
+ */
